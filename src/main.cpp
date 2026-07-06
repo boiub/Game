d09f396cc08c242fc59c2f8d1de20b7f61337207
Michael Karlsson <micka297@student.liu.se>
2026-07-06 18:42:44 +0200

    Add raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,25 @@
-
-#include "main.h"
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World!" << std::endl;
+#include "raylib.h"
+
+#include "main.h"
+
+int main()
+{
+    InitWindow(1080, 720, "Raylib works!");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("Hello, World!", 50, 100, 24, BLACK);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
+
